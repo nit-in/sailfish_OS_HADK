5f276be8bae8fcd72b07bb896c4bd713d62f27f2
nit-in <nit_in@live.com>
2023-11-30 14:55:31 +0530

    fix typos

--- set_repos.sh
+++ set_repos.sh
@@ -10,8 +10,7 @@ chmod a+rx ~/.bin/repo
 
 checkout_hadk_repos(){
 
-repo init 
-repo sync
+
 repo init --depth=1 --no-repo-verify -u git://github.com/mer-hybris/android.git -b hybris-16.0 -g default,-device,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 rm -rf .repo/project-objects
