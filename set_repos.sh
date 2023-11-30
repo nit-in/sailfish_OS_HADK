ddf45d7e44c8f2aed6940fd34f6d4a71f19be9f0
nit-in <nit_in@live.com>
2023-11-30 14:33:51 +0530

    set repos

---
+++ set_repos.sh
@@ -0,0 +1,22 @@
+install_repo(){
+
+mkdir -p ~/.bin
+PATH="${HOME}/.bin:${PATH}"
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
+chmod a+rx ~/.bin/repo
+	
+}
+
+
+checkout_hadk_repos(){
+
+repo init 
+repo sync
+repo init --depth=1 --no-repo-verify -u git://github.com/mer-hybris/android.git -b hybris-16.0 -g default,-device,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf .repo/project-objects
+
+}
+
+install_repo
+checkout_hadk_repos
